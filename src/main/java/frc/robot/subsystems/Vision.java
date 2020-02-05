4229e9507eb41a442c0127f642f6fddcd17b6de3
Lorenzo <awesomelorenzokwenda@gmail.com>
2020-02-05 17:08:40 +0000

    updated vision (blocks and color)

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -9,12 +9,13 @@ package frc.robot.subsystems;
 
 import java.util.ArrayList;
 
-import edu.wpi.first.wpilibj.util.Color;
+import java.awt.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.pseudoresonance.pixy2api.Pixy2;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2Video;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.Pixy2Video.RGB;
 import io.github.pseudoresonance.pixy2api.links.Link;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
 
@@ -29,25 +30,26 @@ public class Vision extends SubsystemBase {
     pixy.init();
     pixy = Pixy2.createInstance(link);
     pixyCCC = pixy.getCCC();
-    pixyVideo = pixy(pixy);
+    pixyVideo = pixy2Video(pixy);
   }
   
-  public void getRecentBlocks() {
+  public boolean getRecentBlocks(Color color) {
     // Get data from all blocks and recieve said data
     pixyCCC.getBlocks(true, 255, 255);
     ArrayList<Block> blocks = pixyCCC.getBlocks();
 
-    // Send largest blocks to a method to get their color
-    for (int i = 0; i < 5; i++) {
-      getBlockColor(blocks.get(i));
-    }
+    // Send gets data from largest block (color)
+    Color myColor = getBlockColor(blocks.get(0));
+    return(myColor.equals(color));
   }
 
+  // Gets a color for a block
   public Color getBlockColor(Block block) {
     int x = block.getX();
     int y = block.getY();
-    pixyVideo.
-    return null;
+    RGB myRGB = pixyVideo.new RGB(0, 0, 0);
+    pixyVideo.getRGB(x, y, myRGB, false);
+    return myRGB.getColor();
   }
 
   @Override
